0402a5b62082bde9ea1ad35d3ed4e1477d3fb9f1
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-01-27 12:52:07 +0200

    +

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
-// Add imports above this line
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
 console.log(galleryItems);
+
+// 1) Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+const galleryContainerItems = document.querySelector('.gallery');
+const createGalleryItems = galleryItemsCardsMarkup(galleryItems);
+galleryContainerItems.insertAdjacentHTML('afterbegin', createGalleryItems);
+
+function galleryItemsCardsMarkup(galleryItems) {
+	return galleryItems
+		.map(({ description, original, preview}) => {
+			return `
+			<a class="gallery__item" href="${original}">
+			<img class="gallery__image" 
+			src="${preview}" 
+			alt="${description}" />
+		</a>
+		`;
+	})
+		.join('');
+	}
+
+	// 2) Ініціалізація бібліотеки після створення і додання елементів галереї у div.gallery.
+const lightbox = new SimpleLightbox('.gallery a', {
+	captionsData:'alt',
+	captionPosition:'bottom',
+	captionDelay:'250'
+});
