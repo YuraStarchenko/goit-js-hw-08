b065fd6e23bbe3d42417bd1ea3795061e3cfa0d5
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-01-29 16:10:43 +0200

    +

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
+import '../css/common.css';
+import '../css/03-feedback.css';
+// 1) Відстежуй на формі подію input, і щоразу записуй у локальне сховище об'єкт
+//    з полями email і message, у яких зберігай поточні значення полів форми.
+//    Нехай ключем для сховища буде рядок "feedback-form-state".
 
+const refs = {
+	form: document.querySelector('.feedback-form'),
+	textarea: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', onTextareaInput);
+
+function onFormSubmit(e){
+	e.preventDefault();
+	e.current.reset();
+}
+
+function onTextareaInput(e){
+	const message = e.currentTarget.value;
+	localStorage.setItem("feedback-form-state", message);
+}
+
+function popTextarea(){
+
+}
