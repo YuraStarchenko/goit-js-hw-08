6e83d94f0ebddf650b2623e6458157493cf19f2f
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-01-29 14:21:38 +0000

    Deploying to gh-pages from @ YuraStarchenko/goit-js-hw-08@8ac3d7d814271a942b4538cb0ec5875f035ae872 🚀

--- 03-feedback.ab4d261b.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={form:document.querySelector(".feedback-form"),textarea:document.querySelector(".feedback-form textarea")};e.form.addEventListener("submit",(function(e){e.preventDefault(),e.current.reset()})),e.textarea.addEventListener("input",(function(e){var t=e.currentTarget.value;localStorage.setItem("feedback-form-state",t)}))}();
-//# sourceMappingURL=03-feedback.ab4d261b.js.map
