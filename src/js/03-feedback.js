319f623929cb37111a9067b908b367302b3a64c5
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-01-30 16:20:55 +0200

    +

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,2 +1,41 @@
 import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = "feedback-form-state";
+
+const refs = {
+	form: document.querySelector('.feedback-form'),
+	textarea: document.querySelector('.feedback-form textarea'),
+};
+
+const formData = {};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', throttle(onFormInput, 500));
+
+refs.form.addEventListener('input', e => {
+	formData[e.target.name] = e.target.value;
+	localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+});
+
+
+popTextarea();
+
+function onFormSubmit(e){
+	e.preventDefault();
+	e.target.reset();
+	localStorage.removeItem(STORAGE_KEY);
+}
+
+function onFormInput(e){
+	const message = e.target.value;
+
+	localStorage.setItem(STORAGE_KEY, message);
+}
+
+function popTextarea(){
+	const saveMessage = localStorage.getItem(STORAGE_KEY);
+
+	if(saveMessage){
+		refs.textarea.value = saveMessage;
+	}
+}
