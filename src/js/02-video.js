08820c544e5a87e8bdb9ba37393e187bac60313b
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-01-30 21:44:33 +0200

    +

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,13 @@
 import Player from "@vimeo/player";
-const throttle = require("lodash.throttle");
+import throttle from "lodash.throttle";
 
-const iframe = document.querySelector('iframe');
+const TIME_VIDEO = "videoplayer-current-time";
+
+const iframe = document.getElementById('vimeo-player');
 const player = new Player(iframe);
 
-player.on('play', function() {
-	console.log('played the video!');
-});
+player.on('timeupdate', throttle(function(e) {
+	localStorage.setItem(TIME_VIDEO, e.seconds);
+}, 1000)
+);
+player.setCurrentTime(localStorage.getItem(TIME_VIDEO));
